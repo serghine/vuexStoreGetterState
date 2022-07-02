f08e0e50255379685bd4294210f0ac9fe39a9700
serghine <sergh.boudj@gmail.com>
2022-07-02 12:24:19 +0200

    [vuex-learn-project] initialisation

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="counter">
+    {{counter}}
+  </div>
+  <div class="buttons">
+    <button>+</button>
+    <button>-</button>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+// Compostant
+components: {},
+
+// Nom de l'application
+name: 'App',
+
+// Data
+data(){
+    return{
+      counter:0
+    }
+  }
+
+}
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+};
+div{
+  margin-bottom: 10px;
+}
+.counter{
+  font-size: 80px;
+}
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+
+
+
+</style>
